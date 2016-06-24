1434effaa3ac9063560256939d6ccea085271682
chenweijieNo1 <372015086@qq.com>
2016-06-24 11:56:19 +0800

    add chenweijie2

--- app/src/main/java/com/example/ericchen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ericchen/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
+        System.out.println("testchenweijie2");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
